d034ff021e708a4abfed2aa1b0102bc9dff217ba
bashiru98 <atulebashirubukari@gmail.com>
2021-08-14 02:13:23 +0300

    added update

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const { ApolloServer } = require('apollo-server');
+const { connect } = require("mongoose")
+const typeDefs =require('./graphql/typeDefs');
+const resolvers =require('./graphql/resolvers');
+const { MONGODB } =require('./config.js')
 
-const pubsub = new PubSub();
 
 const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub })
+  context: ({ req }) => ({ req })
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
     return server.listen({ port: PORT });
