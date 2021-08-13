dca9e15ec6b245d7e8c2ec491176e807267ba5c1
bashiru98 <atulebashirubukari@gmail.com>
2021-08-13 21:14:13 +0300

    added sample code

---
+++ index.js
@@ -0,0 +1,27 @@
+const  { ApolloServer } = require("apollo-server")
+
+const gql = require("graphql-tag")
+
+const typeDefs = gql`
+type Query {
+
+    Hi:String!
+}
+`
+
+const resolvers = {
+    Query :{
+        Hi:() => "hello world"
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen({
+    port:5000
+}).then(res => {
+    console.log(`server startes on ${res.url}`)
+})
